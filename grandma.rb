94f5b4083ad55eafa5f27d6290e51e75d35e11f8
Connor Cowan <ccowan003@comcast.net>
2019-05-29 23:18:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
+  if phrase == "I LOVE YOU GRANDMA!"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
+  else phrase != phrase.upcase
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
